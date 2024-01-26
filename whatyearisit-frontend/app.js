79ef6dc3250f05c196f388cbad7ca57529d4c663
yanis <layrac.yanis@gmail.com>
2024-01-26 10:38:23 +0100

    message

--- whatyearisit-frontend/app.js
+++ whatyearisit-frontend/app.js
@@ -1,8 +1,7 @@
 
-fetch('https://whatyearisit-dusky.vercel.app/year')
+fetch('whatyearisit-l2puhkprk-yanis-projects-a4beb062.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#year').textContent = data.year;
     })
-
 
